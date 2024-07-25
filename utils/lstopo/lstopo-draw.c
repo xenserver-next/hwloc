ab00e7b465629b32f8ed931f3776bea57c4fa5ce
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-07-25 12:00:00 +0200

    Xen topology support v1.1.1: NUMA PCI topology and a die per NUMA node
    - Use PCI device strings from libpciaccess for text and SVG output
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- utils/lstopo/lstopo-draw.c
+++ utils/lstopo/lstopo-draw.c
@@ -981,12 +981,44 @@ lstopo_obj_snprintf(struct lstopo_output *loutput, char *text, size_t textlen, h
       return hwloc_obj_type_snprintf(text, textlen, obj, 0);
     }
   }
+  /* Instead of prepending just "PCI" before the PCI bus, use PCI Class: */
+  if (obj->type == HWLOC_OBJ_PCI_DEVICE)
+    return snprintf(text, textlen, "%s:",
+      hwloc_pci_class_string(obj->attr->pcidev.class_id));
 
   /* subtype replaces the basic type name */
   if (obj->subtype) {
     snprintf(typestr, sizeof(typestr), "%s", obj->subtype);
   } else {
     hwloc_obj_type_snprintf(typestr, sizeof(typestr), obj, 0);
+  }
+  /* Instead of prepending just "PCI" before the PCI bus, use PCI Class: */
+  if (obj->type == HWLOC_OBJ_PCI_DEVICE) {
+    /* If the PCI backend added PCI device strings, include them in the SVG */
+    char *d, *dev = strdup(hwloc_obj_get_info_by_name(obj, "PCIDevice"));
+    if (dev) {
+      char *v, *vendor = strdup(hwloc_obj_get_info_by_name(obj, "PCIVendor"));
+
+      if (!(d = strstr(dev, " &")))
+        d = index(dev, '&');  // Forbidden in SVG
+      if (d) *d = '\0';
+      d = strstr(dev, " and");  // Shorten too verbose strings
+      if (d) *d = '\0';
+      d = strstr(dev, " Ethernet");
+      if (d) *d = '\0';
+      d = strstr(dev, " Gigabit");
+      if (d) *d = '\0';
+      v = strstr(vendor, " ");
+      if (v) *v = '\0';
+      if (!strcmp("Hewlett-Packard", vendor))
+        vendor = (char *)"HP";
+      if (!strncmp("Integrated ", dev, strlen("Integrated ")))
+        dev += strlen("Integrated ");
+      return snprintf(text, textlen, "%s (%s %s):",
+        hwloc_pci_class_string(obj->attr->pcidev.class_id), vendor, dev);
+    }
+    return snprintf(text, textlen, "%s:",
+      hwloc_pci_class_string(obj->attr->pcidev.class_id));
   }
 
   if (index_type == LSTOPO_INDEX_TYPE_DEFAULT) {
